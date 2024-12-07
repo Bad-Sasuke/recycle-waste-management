f370740367c3067d0908d6636b1394616381f13b
watchakorn-18k <wk18k@proton.me>
2024-12-08 05:24:55 +0700

    fix : change layout for responsive

--- frontend/src/components/MarketplacePage.vue
+++ frontend/src/components/MarketplacePage.vue
@@ -183,7 +183,9 @@ const openModalWaste = () => {
       </div>
 
       <!-- รายการราคาขยะ -->
-      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
+      <div
+        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 xl:grid-cols-7 gap-6"
+      >
         <div v-for="(item, index) in filteredItems" :key="index" class="flex justify-center">
           <Card
             :id="item.waste_id"
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,6 +8,20 @@ export default {
         'kanit': ['Kanit', 'sans-serif'],
       },
     },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '960px',
+
+      'lg': '1440px',
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
   },
   plugins: [daisyui],
   daisyui: {
